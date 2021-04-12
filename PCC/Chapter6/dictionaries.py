0b5c27161346f6de8dd6e1183bcc207de15eef20
suvo-oko <miravalles@gmail.com>
2021-04-12 11:48:46 +0200

    Update chapter 6 of PCC

--- PCC/Chapter6/dictionaries.py
+++ PCC/Chapter6/dictionaries.py
@@ -7,3 +7,42 @@
 alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])  # this will get us the value associated with the color key from the alien_0 dictionary
+
+# To get a value from the dictionary.
+
+print(alien_0['color'])
+
+new_points = alien_0['points']
+
+print(f"You just earned {new_points} points!")
+
+# Dictionaries are dynamic structures, meaning we can add, replace or remove key-value pairs.
+
+alien_0['x_position'] = 0  # here we're adding new key-value pairs
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0['color'] = 'yellow'  # here we're replacing an existing value with a new one
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+# We can write a dictionary like this:
+
+favorite_languages = {
+    'Bob': 'Python',
+    'Sarah': 'C',
+    'Joe': 'Ruby',
+    'Mel': 'Python',  # it is common practice to put a comma after the last key-value pair
+    }
+
+language = favorite_languages['Mel']
+print(f"Mel's favorite programming language is {language}.")
+
+# Using the get() method to return a value if the requested key doesn't exist:
+alien_1 = {'color': 'blue', 'speed': 'slow'}
+
+point_value = alien_1.get('points', 'No point value assigned.')
+# if the 'points' key doesn't exist in the alien_1 dictionary, the program will
+# return 'No point value assigned'.
---
+++ PCC/Chapter6/exercise6-1.py
@@ -0,0 +1,13 @@
+# A dict to store a friend's personal information.
+
+friend = {
+    'first_name': 'Bob',
+    'last_name': 'Dylan',
+    'age': 79,
+    'city': 'Malibu',
+    }
+
+print("My friend's name is", friend['first_name'], friend['last_name'])
+print("He's", friend['age'], "and lives in", friend['city'])
+
+print(f"My friend's name is {friend['first_name']} {friend['last_name']}.")
